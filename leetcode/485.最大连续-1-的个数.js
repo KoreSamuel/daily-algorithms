7311da11633b19458adba70e672d6473db23bd4a
swust-xiaoj <swustxiaojie@163.com>
2019-07-10 23:03:27 +0800

    add 485

---
+++ leetcode/485.最大连续-1-的个数.js
@@ -0,0 +1,24 @@
+/*
+ * @lc app=leetcode.cn id=485 lang=javascript
+ *
+ * [485] 最大连续1的个数
+ */
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxConsecutiveOnes = function(nums) {
+  let max = 0;
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] === 1) {
+      count += 1;
+      if (count > max) {
+        max = count;
+      }
+    } else {
+      count = 0;
+    }
+  }
+  return max;
+};
