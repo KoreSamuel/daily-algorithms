f66329804f7acc72289cabc5e034dc812ba75722
KoreSamuel <swustxiaojie@163.com>
2019-08-27 22:14:49 +0800

    add 404

---
+++ leetcode/404.左叶子之和.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=404 lang=javascript
+ *
+ * [404] 左叶子之和
+ */
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+  if (root === null) {
+    return 0;
+  }
+  if (
+    root.left !== null &&
+    root.left.left === null &&
+    root.left.right === null
+  ) {
+    return root.left.val + sumOfLeftLeaves(root.right);
+  } else {
+    return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+  }
+};
