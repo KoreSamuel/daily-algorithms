f6991115c2a17c572c961b7325ff7611e66a0495
KoreSamuel <swustxiaojie@163.com>
2019-08-28 22:37:09 +0800

    add 509

---
+++ leetcode/509.斐波那契数.js
@@ -0,0 +1,16 @@
+/*
+ * @lc app=leetcode.cn id=509 lang=javascript
+ *
+ * [509] 斐波那契数
+ */
+/**
+ * @param {number} N
+ * @return {number}
+ */
+var fib = function(N) {
+  if (N === 0 || N === 1) {
+    return N;
+  }
+  return fib(N - 1) + fib(N - 2);
+};
+// console.log(fib(4));
