204af94914b4f3ddd573fedeeb4926a751a31320
KoreSamuel <swustxiaojie@163.com>
2020-05-04 16:57:10 +0800

    update [62] 不同路径

--- leetcode/62.不同路径.js
+++ leetcode/62.不同路径.js
@@ -10,19 +10,30 @@
  * @param {number} n
  * @return {number}
  */
+// var uniquePaths = function (m, n) {
+//   const dp = new Array(m + 1).fill(new Array(n + 1));
+//   dp[1][1] = 0;
+//   for (let i = 1; i <= m; i++) {
+//     for (let j = 1; j <= n; j++) {
+//       if (i === 1 || j === 1) {
+//         dp[i][j] = 1;
+//       } else {
+//         dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+//       }
+//     }
+//   }
+//   return dp[m][n];
+// };
 var uniquePaths = function (m, n) {
-  const dp = new Array(m + 1).fill(new Array(n + 1));
-  dp[1][1] = 0;
-  for (let i = 1; i <= m; i++) {
-    for (let j = 1; j <= n; j++) {
-      if (i === 1 || j === 1) {
-        dp[i][j] = 1;
-      } else {
-        dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
-      }
-    }
+  let N = m + n - 2;
+  let K = n - 1;
+  let sum = 1;
+  let temp = 1;
+  while (K) {
+    sum *= N--;
+    temp *= K--;
   }
-  return dp[m][n];
+  return sum / temp;
 };
 // @lc code=end
 
