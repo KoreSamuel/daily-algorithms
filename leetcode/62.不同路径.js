de445740fe3a5ee698de44d36d8f165a57d0458c
KoreSamuel <swustxiaojie@163.com>
2020-05-04 16:39:53 +0800

    add [62] 不同路径

---
+++ leetcode/62.不同路径.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=62 lang=javascript
+ *
+ * [62] 不同路径
+ */
+
+// @lc code=start
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+  const dp = new Array(m + 1).fill(new Array(n + 1));
+  dp[1][1] = 0;
+  for (let i = 1; i <= m; i++) {
+    for (let j = 1; j <= n; j++) {
+      if (i === 1 || j === 1) {
+        dp[i][j] = 1;
+      } else {
+        dp[i][j] = dp[i][j - 1] + dp[i - 1][j];
+      }
+    }
+  }
+  return dp[m][n];
+};
+// @lc code=end
+
