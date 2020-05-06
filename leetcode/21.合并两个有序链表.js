fc86c49549735d2ae7707ff25d0fa00f13bcbc46
KoreSamuel <swustxiaojie@163.com>
2020-05-06 22:41:37 +0800

    update [21] 合并两个有序链表

--- leetcode/21.合并两个有序链表.js
+++ leetcode/21.合并两个有序链表.js
@@ -15,14 +15,34 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-var mergeTwoLists = function(l1, l2) {
-  if (l1 === null) return l2;
-  if (l2 === null) return l1;
-  if (l1.val < l2.val) {
-    l1.next = mergeTwoLists(l1.next, l2);
-    return l1;
-  } else {
-    l2.next = mergeTwoLists(l1, l2.next);
-    return l2;
+// var mergeTwoLists = function(l1, l2) {
+//   if (l1 === null) return l2;
+//   if (l2 === null) return l1;
+//   if (l1.val < l2.val) {
+//     l1.next = mergeTwoLists(l1.next, l2);
+//     return l1;
+//   } else {
+//     l2.next = mergeTwoLists(l1, l2.next);
+//     return l2;
+//   }
+// };
+
+var mergeTwoLists = function (l1, l2) {
+
+  let ph = new ListNode(0);
+  let l = ph;
+
+  while (l1 !== null && l2 !== null) {
+    if (l1.val < l2.val) {
+      l.next = l1;
+      l1 = l1.next;
+    } else {
+      l.next = l2;
+      l2 = l2.next;
+    }
+    l = l.next
   }
-};
+
+  l.next = l1 === null ? l2 : l1;
+  return ph.next;
+}
