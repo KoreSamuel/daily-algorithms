658582ddcab36f2233afc75498afd4c16bf100de
KoreSamuel <swustxiaojie@163.com>
2020-05-06 22:58:41 +0800

    update [876] 链表的中间结点

--- leetcode/876.链表的中间结点.js
+++ leetcode/876.链表的中间结点.js
@@ -16,19 +16,30 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
-var middleNode = function (head) {
-  let count = 1;
-  let p = head;
-  while (p.next) {
-    count++;
-    p = p.next;
-  }
-  count = Math.floor(count / 2);
-  while (count--) {
-    head = head.next;
-  }
+// 单指针
+// var middleNode = function (head) {
+//   let count = 1;
+//   let p = head;
+//   while (p.next) {
+//     count++;
+//     p = p.next;
+//   }
+//   count = Math.floor(count / 2);
+//   while (count--) {
+//     head = head.next;
+//   }
 
-  return head;
-};
+//   return head;
+// };
+
+// 双指针
+var middleNode = function (head) {
+  let l1 = l2 = head;
+  while (l2 && l2.next) {
+    l1 = l1.next;
+    l2 = l2.next.next;
+  }
+  return l1;
+}
 // @lc code=end
 
