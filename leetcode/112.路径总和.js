94252f592794ae1e09d1afdf949eac3ad805fc80
KoreSamuel <swustxiaojie@163.com>
2020-05-12 22:34:47 +0800

    update [112] 路径总和

--- leetcode/112.路径总和.js
+++ leetcode/112.路径总和.js
@@ -17,13 +17,26 @@
  * @param {number} sum
  * @return {boolean}
  */
-var hasPathSum = function (root, sum) {
-  if (root === null) return false;
-  sum -= root.val;
-  if (root.left === null && root.right === null) {
-    return sum === 0
+// 递归
+// var hasPathSum = function (root, sum) {
+//   if (root === null) return false;
+//   sum -= root.val;
+//   if (root.left === null && root.right === null) {
+//     return sum === 0
+//   }
+//   return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
+// };
+// 迭代
+var hasPathSum = function (root, num) {
+  if (!root) return false;
+  const queue = [[root, num - root.val]];
+  while (queue.length) {
+    let [node, curNum] = queue.shift();
+    if (node.left) queue.push([node.left, curNum - node.left.val]);
+    if (node.right) queue.push([node.right, curNum - node.right.val]);
+    if (!node.left && !node.right && curNum === 0) return true;
   }
-  return hasPathSum(root.left, sum) || hasPathSum(root.right, sum);
-};
+  return false;
+}
 // @lc code=end
 
