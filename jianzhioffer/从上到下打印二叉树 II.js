dc47cbba292ec726a89080fc529d5bc7dd64cd02
KoreSamuel <swustxiaojie@163.com>
2020-05-19 23:31:35 +0800

    add 从上到下打印二叉树 II

---
+++ jianzhioffer/从上到下打印二叉树 II.js
@@ -0,0 +1,29 @@
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrder = function (root) {
+  if (!root) return [];
+  const res = [];
+  const queue = [root];
+  while (queue.length) {
+    let temp = [];
+    let len = queue.length;
+    while (len--) {
+      let cur = queue.shift();
+      temp.push(cur.val);
+      if (cur.left) queue.push(cur.left);
+      if (cur.right) queue.push(cur.right);
+    }
+    res.push(temp);
+  }
+  return res;
+};
