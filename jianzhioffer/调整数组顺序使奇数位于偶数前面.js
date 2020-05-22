82e9d303bcc282bcdfdf4e1e64ea6065d0910795
KoreSamuel <swustxiaojie@163.com>
2020-05-22 23:56:43 +0800

    update 调整数组顺序使奇数位于偶数前面

--- jianzhioffer/调整数组顺序使奇数位于偶数前面.js
+++ jianzhioffer/调整数组顺序使奇数位于偶数前面.js
@@ -5,3 +5,25 @@
 var exchange = function (nums) {
   return nums.filter(n => n % 2 === 1).concat(nums.filter(n => n % 2 === 0));
 };
+
+var exchange = function (nums) {
+  let l = 0, r = nums.length - 1;
+  let t = undefined;
+  while (l < r) {
+    if (nums[l] % 2 !== 0) {
+      l++;
+      continue;
+    }
+    if (nums[r] % 2 !== 1) {
+      r--;
+      continue;
+    }
+    t = nums[l];
+    nums[l] = nums[r];
+    nums[r] = t;
+    l++;
+    r--;
+  }
+  return nums;
+};
+console.log(exchange([2, 16, 3, 5, 13, 1, 16, 1, 12, 18, 11, 8, 11, 11, 5, 1]))
