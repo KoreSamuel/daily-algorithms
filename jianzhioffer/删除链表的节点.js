1a99c7db723cb3595a48a91c8a787e840618c018
KoreSamuel <swustxiaojie@163.com>
2020-05-26 22:52:07 +0800

    add 删除链表的节点

---
+++ jianzhioffer/删除链表的节点.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @param {number} val
+ * @return {ListNode}
+ */
+var deleteNode = function (head, val) {
+  while (head && head.val === val) {
+    head = head.next;
+  }
+  let p = head;
+  while (p.next) {
+    if (p.next.val === val) {
+      p.next = p.next.next;
+    } else {
+      p = p.next;
+    }
+  }
+  return head;
+};
