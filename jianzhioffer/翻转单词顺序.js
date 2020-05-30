f90dcc192d2121014e499d6cf09ee7cf5eb66623
KoreSamuel <swustxiaojie@163.com>
2020-05-30 23:27:53 +0800

    update 翻转单词顺序

--- jianzhioffer/翻转单词顺序.js
+++ jianzhioffer/翻转单词顺序.js
@@ -2,18 +2,24 @@
  * @param {string} s
  * @return {string}
  */
+// var reverseWords = function (s) {
+//   let res = '';
+//   let temp = '';
+//   for (let i = 0; i < s.length; i++) {
+//     if (s[i] !== ' ') {
+//       temp += s[i];
+//     } else if (temp !== '') {
+//       res = res.length ? temp + ' ' + res : temp;
+//       temp = '';
+//     } else {
+//       continue;
+//     }
+//   }
+//   return temp.length ? (res.length ? temp + ' ' + res : temp) : res;
+// };
+
 var reverseWords = function (s) {
-  let res = '';
-  let temp = '';
-  for (let i = 0; i < s.length; i++) {
-    if (s[i] !== ' ') {
-      temp += s[i];
-    } else if (temp !== '') {
-      res = res.length ? temp + ' ' + res : temp;
-      temp = '';
-    } else {
-      continue;
-    }
-  }
-  return temp.length ? (res.length ? temp + ' ' + res : temp) : res;
+  s = s.trim();
+  let sa = s.split(' ').filter(a => a.length);
+  return [...sa].map(() => sa.pop(), sa).join(' ');
 };
