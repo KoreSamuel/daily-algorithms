573af49d72a18fa8417996594fdc1fb1895ff995
KoreSamuel <swustxiaojie@163.com>
2020-05-31 00:11:27 +0800

    add [98] 验证二叉搜索树

---
+++ leetcode/98.验证二叉搜索树.js
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=98 lang=javascript
+ *
+ * [98] 验证二叉搜索树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function (root) {
+  if (!root) return true;
+  const queue = [[Number.MIN_SAFE_INTEGER, Number.MAX_SAFE_INTEGER, root]];
+  while (queue.length) {
+    let [min, max, cur] = queue.shift();
+    if (cur.val <= min || cur.val >= max) {
+      return false;
+    }
+    if (cur.left) queue.push([min, cur.val, cur.left])
+    if (cur.right) queue.push([cur.val, max, cur.right])
+  }
+  return true;
+};
+// @lc code=end
+
