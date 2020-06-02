c8e4bf0d739f8ff1617a7c5005982c72007c77ef
KoreSamuel <swustxiaojie@163.com>
2020-06-02 23:19:14 +0800

    add 二叉搜索树的第k大节点

---
+++ jianzhioffer/二叉搜索树的第k大节点.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} k
+ * @return {number}
+ */
+var kthLargest = function (root, k) {
+  const stack = [];
+  const queue = [];
+  while (stack.length || root !== null) {
+    while (root !== null) {
+      stack.push(root);
+      root = root.left;
+    }
+    root = stack.pop();
+    queue.unshift(root.val);
+    root = root.right;
+  }
+  return queue[k - 1];
+};
