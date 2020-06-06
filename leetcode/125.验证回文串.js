4b8faed87e4e1dd48a2506d7be14ac9ec1e9cb8b
KoreSamuel <swustxiaojie@163.com>
2020-06-07 00:14:12 +0800

    add [125] 验证回文串

---
+++ leetcode/125.验证回文串.js
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=125 lang=javascript
+ *
+ * [125] 验证回文串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  if (!s.length) return true;
+  s = s.replace(/[^0-9a-zA-Z]/g, '').toLocaleLowerCase();
+  let l = 0, r = s.length - 1;
+  while (l < r) {
+    if (s[l] !== s[r]) {
+      return false;
+    }
+    l++;
+    r--;
+  }
+  return true;
+};
+// @lc code=end
+
