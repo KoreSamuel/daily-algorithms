dcaca44a4bddf59452f6b9be07960a2b888db2d2
KoreSamuel <swustxiaojie@163.com>
2020-06-09 23:08:42 +0800

    update 1302. 层数最深叶子节点的和

--- leetcode/1302. 层数最深叶子节点的和.js
+++ leetcode/1302. 层数最深叶子节点的和.js
@@ -9,6 +9,7 @@
  * @param {TreeNode} root
  * @return {number}
  */
+// BFS 保存了每层节点的值，最后将最后一层相加
 var deepestLeavesSum = function (root) {
   const res = [];
   const queue = [root];
@@ -23,5 +24,41 @@ var deepestLeavesSum = function (root) {
     }
     res.push(temp);
   }
-  return res.pop().reduce((acc, cur) => acc + cur, 0)
+  return res.pop().reduce((acc, cur) => acc + cur, 0);
 };
+
+// BFS 每层都相加，下一层将上一层覆盖，最后返回的就是最后一层的
+var deepestLeavesSum = function (root) {
+  const queue = [root];
+  let res = 0;
+  while (queue.length) {
+    let len = queue.length;
+    res = 0;
+    while (len--) {
+      let cur = queue.shift();
+      if (cur.left) queue.push(cur.left);
+      if (cur.right) queue.push(cur.right);
+      res += cur.val;
+    }
+  }
+  return res;
+};
+
+// DFS
+var deepestLeavesSum = function (root) {
+  let maxLevel = -1;
+  let total = 0;
+  function dfs(node, level) {
+    if (!node) return;
+    if (level > maxLevel) {
+      maxLevel = level;
+      total = node.val;
+    } else if (level === maxLevel) {
+      total += node.val;
+    }
+    dfs(node.left, level + 1);
+    dfs(node.right, level + 1)
+  }
+  dfs(root, 0);
+  return total;
+}
