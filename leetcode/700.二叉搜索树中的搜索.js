45b447dc8b54bea7bf5e342963d856f577450496
KoreSamuel <swustxiaojie@163.com>
2020-06-12 23:35:59 +0800

    add [700] 二叉搜索树中的搜索

---
+++ leetcode/700.二叉搜索树中的搜索.js
@@ -0,0 +1,29 @@
+/*
+ * @lc app=leetcode.cn id=700 lang=javascript
+ *
+ * [700] 二叉搜索树中的搜索
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var searchBST = function (root, val) {
+  if (root === null || root.val === val) return root;
+  if (val < root.val) {
+    return searchBST(root.left, val);
+  } else {
+    return searchBST(root.right, val);
+  }
+};
+// @lc code=end
+
