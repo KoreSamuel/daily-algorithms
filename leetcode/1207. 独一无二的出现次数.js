5e82abe33940f20be6a94dbf502de4cf4cf5cb97
KoreSamuel <swustxiaojie@163.com>
2020-06-28 22:36:37 +0800

    add 1207. 独一无二的出现次数

---
+++ leetcode/1207. 独一无二的出现次数.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var uniqueOccurrences = function (arr) {
+  let m = new Map();
+  for (const n of arr) {
+    m.set(n, (m.get(n) || 0) + 1);
+  }
+  const t = {};
+  for (const n of m.values()) {
+    if (n in t) {
+      return false;
+    }
+  }
+  return true;
+};
