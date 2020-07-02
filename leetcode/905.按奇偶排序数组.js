0e1b719e8c402a5092bd91618c70ed24589d90cf
KoreSamuel <swustxiaojie@163.com>
2020-07-02 21:27:17 +0800

    update [905] 按奇偶排序数组

--- leetcode/905.按奇偶排序数组.js
+++ leetcode/905.按奇偶排序数组.js
@@ -7,6 +7,21 @@
  * @param {number[]} A
  * @return {number[]}
  */
-var sortArrayByParity = function(A) {
+var sortArrayByParity = function (A) {
   return [...A.filter(a => a % 2 === 0), ...A.filter(a => a % 2 !== 0)];
 };
+
+var sortArrayByParity = function (A) {
+  let l = 0, r = A.length - 1;
+  while (l < r) {
+    if (A[l] % 2 > A[r] % 2) {
+      let temp = A[l];
+      A[l] = A[r];
+      A[r] = temp;
+    }
+
+    if (A[l] % 2 === 0) l++;
+    if (A[r] % 2 === 1) r--;
+  }
+  return A;
+}
