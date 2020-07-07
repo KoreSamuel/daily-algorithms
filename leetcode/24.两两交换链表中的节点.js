4ddc3fc4f5df0ba5197445bd1a20b56eaef34bb8
KoreSamuel <swustxiaojie@163.com>
2020-07-07 22:21:58 +0800

    add [24] 两两交换链表中的节点

---
+++ leetcode/24.两两交换链表中的节点.js
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=24 lang=javascript
+ *
+ * [24] 两两交换链表中的节点
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function (head) {
+  if (!head || !head.next) return head;
+  let p = head;
+  let q = head.next;
+  p.next = swapPairs(q.next);
+  q.next = p;
+  return q;
+};
+// @lc code=end
+
